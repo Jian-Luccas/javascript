74017834d4eb0959fb2e8ec87528c1e98acc122e
Jian Luccas <125167296+Jian-Luccas@users.noreply.github.com>
2024-02-01 16:45:46 -0300

    novos exercicios

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+while (c < 3) {
+    console.log('Tudo bem?')
+    c++ // c = c + 1
+}
+
+var d = 1
+while (d <= 5) {
+    console.log(`Passo ${d}`)
+    d++
+}
+
+var e = 1
+do {
+    console.log(`Etapa ${e}`)
+    e++
+} while (e <= 4)
+
+
+/* Repetições Manuais
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,19 @@
+var c = 1
+while (c <= 2) {
+    console.log(c)
+    c++
+}
+
+//-----------------
+
+for(var g = 20; g <= 21; g++) {
+    console.log(g)
+}
+
+//-----------------
+
+console.log('Vai começar...')
+for(var o = 1; o <= 4; o++) {
+    console.log(o)
+}
+console.log('FIM!')
